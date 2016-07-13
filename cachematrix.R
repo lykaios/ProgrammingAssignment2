e0ab040eb83f0a648808b8a149353a8bc6c92850
nate.allen <nate.allen@workday.com>
2016-07-13 08:50:07 +0100

    implemented function caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are designed to demonstrate the scoping functionality of R by
+## setting variables outside their normal context.
 
-## Write a short comment describing this function
-
+# Returns a list of functions to interact with a special matrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+# Calculates the inverse of a matrix, and caches the calculation for later use. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)){
+    message("retreiving from cache")
+    i
+  }
+  else {
+    data <- x$get()
+    i <- solve(data)
+    x$setinv(i)
+    i
+  }
 }
